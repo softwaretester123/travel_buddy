2d897cc1ee0966cd2be6ec0d62024ef40a6a4440
Ashwin Athappan <ashwinathappank@gmail.com>
2021-12-30 19:39:23 +0530

    Changes as of 2-12-21

--- app/src/main/java/com/example/travelbuddy/Home.java
+++ app/src/main/java/com/example/travelbuddy/Home.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,20 +13,14 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.gms.analytics.ecommerce.Product;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
@@ -83,19 +76,14 @@ public class Home extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.miItem1:
-                        Toast.makeText(getApplicationContext(), "Restaurant selected", Toast.LENGTH_SHORT).show();
-                        Intent restaurant = new Intent(getApplicationContext(), Restaurants.class);
-                        startActivity(restaurant);
-                        break;
-                    case R.id.miItem2:
                         Toast.makeText(getApplicationContext(), "Planned Trips selected", Toast.LENGTH_SHORT).show();
                         Intent plannedTrips = new Intent(getApplicationContext(), PlannedTrips.class);
                         startActivity(plannedTrips);
                         break;
-                    case R.id.miItem3:
-                        Toast.makeText(getApplicationContext(), "Reviews selected", Toast.LENGTH_SHORT).show();
-                        Intent reviews = new Intent(getApplicationContext(), Reviews.class);
-                        startActivity(reviews);
+                    case R.id.miItem2:
+                        Toast.makeText(getApplicationContext(), "About Us selected", Toast.LENGTH_SHORT).show();
+                        Intent restaurant = new Intent(getApplicationContext(), AboutUs.class);
+                        startActivity(restaurant);
                         break;
                 }
                 return true;
